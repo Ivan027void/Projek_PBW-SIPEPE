9d9a1b3f4ba974bee32f167f3e8aec23f281a15c
alifannaufally <alifan@mhs.unsyiah.ac.id>
2023-05-04 01:04:58 +0700

    feat: add detailMahasisawa

---
+++ resources/views/mahasiswa/detailMahasiswa.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+
+<br>
+<div>
+<tr>
+          <td>Judul : </td>
+          <td>E - Commerce</td>
+        </tr>
+    <br>
+ <tr>
+          <td>Nama : </td>
+          <td>Ivan</td>
+        </tr>
+
+</div>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@endsection
