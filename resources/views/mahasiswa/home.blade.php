3c5db244b774c3ee6b07c6d5f4a65108539bbc55
alifannaufally <alifan@mhs.unsyiah.ac.id>
2023-05-04 00:26:13 +0700

    docs : modify home mahasiswa

--- resources/views/mahasiswa/home.blade.php
+++ resources/views/mahasiswa/home.blade.php
@@ -25,6 +25,55 @@
                 </div>
             </div>
             <!-- /.row -->
+            <div> <main>
+    <br>
+    <div class="container text-white">
+     
+      <br>
+      <table class="table w-100 text-white text-center">
+        <tr style="background: #60A5FA;">
+          <th>No</th>
+          <th>Penelitian/Judul</th>
+          <th>Dosen</th>
+          <th>Tanggal Pengajuan</th>
+          <th>Status</th>
+        </tr>
+        <tr class="bg-white text-dark">
+          <td>1</td>
+          <td>E-Commerce</td>
+          <td>Bapak Alim Hisbullah</td>
+          <td>10.00</td>
+          <td><i class="fas fa-check-circle text-info"></i></td>
+        </tr>
+        <tr class="text-dark" style="background: #D4E8FF;">
+          <td>2</td>
+          <td>Website Travel</td>
+          <td>Ibu Dalila Husna Yunardi</td>
+          <td>12.00</td>
+          
+          <td><i class="fas fa-check-circle text-info"></i></td>
+        </tr>
+        <tr class="bg-white text-dark">
+          <td>3</td>
+          <td>Website Alumni</td>
+          <td>Ibu Maharini</td>
+          <td>15.00</td>
+         
+          <td>Unfinished</td>
+
+        </tr>
+      </table>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+  </main>
+</div>
         </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
